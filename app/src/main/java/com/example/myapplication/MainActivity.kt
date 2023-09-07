6670f17cb668fbb283d0fbf71ac39886ee020a20
berry <berry@lendit.co.kr>
2023-09-07 15:50:12 +0900

    값 변화에 따라 컴포넌트 재랜더링

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,15 +41,33 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result = 1
+    // 하드코딩 되어있던 result 값을 Roll 버튼을 탭할 경우 재설정되게끔 반응형으로 바꾸어야한다.
+    // remember 컴포저블로 만든다. -> remember 컴포저블은 함수를 전달해야한다.
+    // remember 컴포저블 내부에서는 mutableStateOf() 함수를 전달하고 기본 값인 1 인수를 전달한다.
+    // result 변수값이 업데이트되면 재구성이 트리거되고, result 값이 반영되어 UI가 재랜더링된다.
+
+    // import androidx.compose.runtime.mutableStateOf
+    // import androidx.compose.runtime.remember
+    // import androidx.compose.runtime.getValue
+    // import androidx.compose.runtime.setValue
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(R.drawable.dice_1), contentDescription = result.toString())
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { (1..6).random() }) {
+        Button(onClick = { result = (1..6).random() }) {
             Text(text = stringResource(R.string.roll))
+            println("result $result")
         }
     }
 }
