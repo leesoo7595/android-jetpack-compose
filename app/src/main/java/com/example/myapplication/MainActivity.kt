68f92f2e16a486cb9c01e1cdde2aa87e7d16d3be
berry <berry@lendit.co.kr>
2023-09-07 11:49:08 +0900

    텍스트 컴포저블 추가

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage("Android")
+                    GreetingImage("Android", "Berry")
                 }
             }
         }
@@ -36,7 +41,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingImage(message: String, modifier: Modifier = Modifier) {
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(id = R.drawable.androidparty)
     Box {
         Image(
@@ -47,6 +52,7 @@ fun GreetingImage(message: String, modifier: Modifier = Modifier) {
         )
         Greeting(
             name = message,
+            from = from,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)
@@ -55,26 +61,41 @@ fun GreetingImage(message: String, modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+    ) {
+        Text(
+            text = "Hello $name!",
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.Start)
+        )
+    }
+}
+
 @Preview(showBackground = false)
 @Composable
 fun GreetingCardPreview() {
     MyApplicationTheme {
-        GreetingImage("Android")
+        GreetingImage("Android", "Berry")
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("Android", "Berry")
     }
 }
