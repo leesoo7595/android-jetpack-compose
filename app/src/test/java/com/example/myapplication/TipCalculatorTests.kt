c31e6d2eaec6ede192443795b3980c7e1cad90b5
berry <berry@lendit.co.kr>
2023-09-08 14:14:49 +0900

    로컬 테스트 작성

---
+++ app/src/test/java/com/example/myapplication/TipCalculatorTests.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculateTip_20PercentNoRoundUp() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+
+        assertEquals(expectedTip, actualTip)
+    }
+
+}
